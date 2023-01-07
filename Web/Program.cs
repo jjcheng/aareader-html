8f905b51b63077672f6017a03a11f708c231765c
jjcheng <chengjianjin@gmail.com>
2023-01-08 00:24:42 +0800

    Update Program.cs

--- Web/Program.cs
+++ Web/Program.cs
@@ -13,7 +13,7 @@ if (!app.Environment.IsDevelopment())
     app.UseMiddleware<RequestMiddleware>();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseHttpsRedirection();
+    //app.UseHttpsRedirection();
 }
 else
 {
