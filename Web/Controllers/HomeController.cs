e106ee531f124d4e18072456ba78be4a92074de5
jjcheng <chengjianjin@gmail.com>
2023-01-08 00:20:44 +0800

    init

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index(string culture = "en")
+    {
+        if(culture == "en")
+        {
+            return View("EnIndex");
+        }
+        else
+        {
+            return View("ZhIndex");
+        }
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+}
---
+++ Web/ViewModels/Error/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AAReader.Web.ViewModels.Error;
+
+public struct ErrorViewModel
+{
+    public int? StatusCode { get; set; } = null;
+    public string Message { get; set; } = default!;
+
+    public ErrorViewModel()
+    {
+
+    }
+
+}
